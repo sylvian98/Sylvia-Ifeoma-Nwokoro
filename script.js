e1c416f69b4ec6f90ad8c1f747588c72bc044838
sylvian98 <60930131+sylvian98@users.noreply.github.com>
2024-10-30 13:00:25 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
- let isSticky = false;
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 0) {
-        if (!isSticky) {
-          jQuery('header').css('position', 'fixed');
-          isSticky = true;
-        }
-      } else {
-        jQuery('header').css('position', 'absolute');
-        isSticky = false;
-      }
-    });
-    function openNav() {
-      document.getElementById("myNav").style.width = "50%";
-//      if (window.innerWidth <= 768) { // Check if viewport width is less than or equal to 768px (mobile)
-//        document.getElementById("myNav").style.width="100%" // Add full-width class to openbtn
-//      }
-    }
-    function closeNav() {
-      document.getElementById("myNav").style.width = "0%";
-    }
+//  let isSticky = false;
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY > 0) {
+//         if (!isSticky) {
+//           jQuery('header').css('position', 'fixed');
+//           isSticky = true;
+//         }
+//       } else {
+//         jQuery('header').css('position', 'absolute');
+//         isSticky = false;
+//       }
+//     });
+//     function openNav() {
+//       document.getElementById("myNav").style.width = "50%";
+// //      if (window.innerWidth <= 768) { // Check if viewport width is less than or equal to 768px (mobile)
+// //        document.getElementById("myNav").style.width="100%" // Add full-width class to openbtn
+// //      }
+//     }
+//     function closeNav() {
+//       document.getElementById("myNav").style.width = "0%";
+//     }
+
+
+
